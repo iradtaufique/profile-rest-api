f3a03bf522faffa86a72e0e2e4fd23b545c01248
Taufique <iradukunda.ta@gmail.com>
2020-06-03 15:05:50 +0200

    Create project and configure the app

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\n-t\desktop\python\udemy\profile-rest-api\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
